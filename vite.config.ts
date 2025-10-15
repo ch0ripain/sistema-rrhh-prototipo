d1ca804eb82d83a2b6e8d87c35a2a462a95002e1
Leandro Rufino <leandrix1234@gmail.com>
2025-10-15 16:10:53 -0300

    feat: vite configuration gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/sistema-rrhh-prototipo/',
   build: {
     // Optimizaciones de build
     target: 'es2015',
